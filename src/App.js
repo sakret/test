3f26a83cbda94b9c7bf71e68ccdde52d1c7e6638
Inna <innsnazv@gmail.com>
2018-07-08 11:53:28 +0300

    Это мой первый коммит

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+              <div className="wrapper">
+                <SayFullName name="Anna" surname="Ivanova" link="vk.com" />
+                <SayFullName name="Olga" surname="Petrova" link="#" />
+                <SayFullName name="Julia" surname="Safronova" link="facebook.com" />
+              </div>
+    );
+  }
+}
+
+
+function SayFullName(props) {
+  return (
+      <div>
+      <h1> Моё имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+      </div>
+    )
+}
+
+export default App;
